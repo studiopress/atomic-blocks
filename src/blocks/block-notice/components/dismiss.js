92f383ebc9bf49fd6f5926a6ab9c68e6a9602201
Mike McAlister <helloarray@gmail.com>
2018-02-20 15:36:13 -0600

    Add missing semis

--- src/blocks/block-notice/components/dismiss.js
+++ src/blocks/block-notice/components/dismiss.js
@@ -2,15 +2,15 @@
 
 	$(document).ready(function() {
 
-		const elems = document.querySelectorAll('.block-notice.dismissable[data-id]')
+		const elems = document.querySelectorAll('.block-notice.dismissable[data-id]');
 		elems.forEach(el => {
-			const uid = el.getAttribute( 'data-id' )
+			const uid = el.getAttribute( 'data-id' );
 			if ( ! localStorage.getItem(`notice-${uid}`) ) {
-				el.style.display = 'block'
+				el.style.display = 'block';
 			}
 			el.querySelector('.block-notice-dismiss').addEventListener('click', () => {
-				localStorage.setItem(`notice-${uid}`, 1)
-				el.style.display = ''
+				localStorage.setItem(`notice-${uid}`, 1);
+				el.style.display = '';
 			})
 		})
 
