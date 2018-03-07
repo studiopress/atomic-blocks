accc00adc947b3d36db2247c956fb8bb0b87a769
Mike McAlister <helloarray@gmail.com>
2018-03-06 22:13:24 -0600

    Move CPTs

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://atomicblocks.com
  * Description: A beautiful collection of WordPress editor blocks to help you effortlessly build the website you've always wanted.
  * Author: atomicblocks
- * Author URI: http://atomicblocks.com
+ * Author URI: http://arraythemes.com
  * Version: 1.0.0
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -24,15 +24,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Initialize the blocks
  */
-function atomic_block_loader() {
+function atomic_blocks_loader() {
+	/**
+	 * Load the blocks functionality
+	 */
 	require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+
+	/**
+	 * Register the custom post types. Still in development.
+	 */
+	// require_once plugin_dir_path( __FILE__ ) . 'src/custom-post-types/testimonials.php';
+	// require_once plugin_dir_path( __FILE__ ) . 'src/custom-post-types/pricing-tables.php';
+	// require_once plugin_dir_path( __FILE__ ) . 'src/custom-post-types/team-members.php';
+
+	/**
+	 * Load plugin textdomain
+	 */
+	load_plugin_textdomain( 'atomic-blocks', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 }
-add_action( 'init', 'atomic_block_loader' );
-
-
-/**
- * Register the custom post types
- */
-require_once plugin_dir_path( __FILE__ ) . 'src/custom-post-types/testimonials.php';
-require_once plugin_dir_path( __FILE__ ) . 'src/custom-post-types/pricing-tables.php';
-require_once plugin_dir_path( __FILE__ ) . 'src/custom-post-types/team-members.php';
+add_action( 'plugins_loaded', 'atomic_blocks_loader' );
