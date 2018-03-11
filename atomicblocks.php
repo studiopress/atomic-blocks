2a3feea01d41156766513bc3abac0bdcea9e0e3f
Mike McAlister <helloarray@gmail.com>
2018-03-11 15:25:00 -0500

    Rewire the redirect

--- atomicblocks.php
+++ atomicblocks.php
@@ -34,11 +34,6 @@ function atomic_blocks_loader() {
 	 * Load Getting Started page
 	 */
 	require_once plugin_dir_path( __FILE__ ) . 'dist/getting-started/getting-started.php';
-
-	/**
-	 * Load plugin textdomain
-	 */
-	load_plugin_textdomain( 'atomic-blocks', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
 }
 add_action( 'plugins_loaded', 'atomic_blocks_loader' );
 
@@ -53,16 +48,23 @@ add_action( 'init', 'atomic_blocks_init' );
 
 
 /**
- * Redirect to the Atomic Blocks Getting Started page on single plugin activation
+ * Add a check for our plugin before redirecting
  */
 function atomic_blocks_activate() {
-	add_option( 'atomic_blocks_do_activation_redirect', true );
-	
-	if ( get_option( 'atomic_blocks_do_activation_redirect', false ) ) {
+    add_option( 'atomic_blocks_do_activation_redirect', true );
+}
+register_activation_hook( __FILE__, 'atomic_blocks_activate' );
+
+
+/**
+ * Redirect to the Atomic Blocks Getting Started page on single plugin activation
+ */
+function atomic_blocks_redirect() {
+    if ( get_option( 'atomic_blocks_do_activation_redirect', false ) ) {
         delete_option( 'atomic_blocks_do_activation_redirect' );
         if( !isset( $_GET['activate-multi'] ) ) {
             wp_redirect( "admin.php?page=atomic-blocks" );
         }
     }
 }
-register_activation_hook( __FILE__, 'atomic_blocks_activate' );
+add_action( 'admin_init', 'atomic_blocks_redirect' );
