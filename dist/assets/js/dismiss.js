bbe6ab248a9df2f1769bc8a56c8c7af90fc50c93
Mike McAlister <helloarray@gmail.com>
2018-04-04 23:38:08 -0500

    Fix the notice selector in js

--- dist/assets/js/dismiss.js
+++ dist/assets/js/dismiss.js
@@ -2,13 +2,13 @@
 
 	$(document).ready(function() {
 
-		const elems = document.querySelectorAll('.block-notice.dismissable[data-id]');
+		const elems = document.querySelectorAll('.ab-block-notice.ab-dismissable[data-id]');
 		elems.forEach(el => {
 			const uid = el.getAttribute( 'data-id' );
 			if ( ! localStorage.getItem(`notice-${uid}`) ) {
 				el.style.display = 'block';
 			}
-			el.querySelector('.block-notice-dismiss').addEventListener('click', () => {
+			el.querySelector('.ab-notice-dismiss').addEventListener('click', () => {
 				localStorage.setItem(`notice-${uid}`, 1);
 				el.style.display = '';
 			})
