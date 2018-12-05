cd5869282aa48bc63e6c8f59f256b4453c47d0cb
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2018-12-05 18:10:32 -0300

    updating version

--- lsx-blocks.php
+++ lsx-blocks.php
@@ -5,7 +5,7 @@
  * Description: LSX Blocks is a Gutenberg plugin created via create-guten-block.
  * Author: lightspeed
  * Author URI: https://AhmadAwais.com/
- * Version: 1.0.0
+ * Version: 1.0.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
--- readme.txt
+++ readme.txt
@@ -5,5 +5,5 @@ Tags: lsx, gutenberg, blocks
 Requires at least: 4.3
 Tested up to: 4.8
 Requires PHP: 7.0
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 License: GPLv3
