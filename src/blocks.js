5e6cc1215122b0fbd85a84f2a727fc1c5c2501f2
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2018-12-06 16:26:53 -0300

    Adding the Blocks posts block

--- src/blocks.js
+++ src/blocks.js
@@ -14,3 +14,4 @@ import './cta-block/cta-block.js';
 import './faq-block/faq-block.js';
 import './team-block/team-block.js';
 import './testimonials-block/testimonials-block.js';
+import './post-block/post-block.js';
