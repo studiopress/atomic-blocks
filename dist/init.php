82a0d0e1241526ed9d410c758dddd14641765142
Mike McAlister <m@mikemcalister.com>
2019-03-04 15:23:21 -0600

    Remove FontAwesome from front end
    Fixes #68

--- dist/init.php
+++ dist/init.php
@@ -29,14 +29,6 @@ function atomic_blocks_block_assets() {
 		array(),
 		filemtime( plugin_dir_path( __FILE__ ) . 'blocks.style.build.css' )
 	);
-
-	// Load the FontAwesome icon library
-	wp_enqueue_style(
-		'atomic-blocks-fontawesome',
-		plugins_url( 'dist/assets/fontawesome/css/all' . $postfix . '.css', dirname( __FILE__ ) ),
-		array(),
-		filemtime( plugin_dir_path( __FILE__ ) . 'assets/fontawesome/css/all.css' )
-	);
 }
 add_action( 'init', 'atomic_blocks_block_assets' );
 
