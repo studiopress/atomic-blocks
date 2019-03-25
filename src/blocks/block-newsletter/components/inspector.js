5bba912e3533ee5fd4a432676f89f49b3a142d3f
Mike McAlister <m@mikemcalister.com>
2019-03-25 09:26:28 -0500

    Reformat color array

--- src/blocks/block-newsletter/components/inspector.js
+++ src/blocks/block-newsletter/components/inspector.js
@@ -158,10 +158,9 @@ class Inspector extends Component {
 
 export default compose( [
 	applyFallbackStyles,
-	withColors( 'backgroundColor', {
-		textColor: 'color',
-		buttonBackgroundColor: 'background-color',
-		buttonTextColor: 'color',
-	},
+	withColors( 'backgroundColor',
+	{ textColor: 'color' },
+	{ buttonBackgroundColor: 'background-color' },
+	{ buttonTextColor: 'color' },
 	),
 ] )( Inspector );
