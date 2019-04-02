80e4d6b5c8241c10de9defba8213d2e54a40f7c3
Mike McAlister <m@mikemcalister.com>
2019-04-01 22:34:06 -0500

    Add link to Mailchimp api docs.

--- dist/getting-started/pages/settings-general.php
+++ dist/getting-started/pages/settings-general.php
@@ -17,6 +17,14 @@ $atomic_blocks_mailchimp_api_key = get_option( 'atomic_blocks_mailchimp_api_key'
 			</th>
 			<td>
 				<input type="text" id="atomic-blocks-settings[mailchimp-api-key]" name="atomic-blocks-settings[mailchimp-api-key]" size="40" value="<?php echo esc_attr( $atomic_blocks_mailchimp_api_key ); ?>" />
+				<?php
+					$mailchimp_link = sprintf(
+						'<p><a href="%1$s" target="_blank">%2$s</a></p>',
+						esc_url( 'https://mailchimp.com/help/about-api-keys/' ),
+						__( 'Find your Mailchimp API key.', 'atomic-blocks' )
+					);
+					echo $mailchimp_link;
+				?>
 			</td>
 		</tr>
 	</tbody>
