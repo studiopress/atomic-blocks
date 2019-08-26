f52e14be69b8633807b5da07c6f1fc5df5e40210
Mike McAlister <m@mikemcalister.com>
2019-08-26 15:03:39 -0500

    Fix background image opacity on column block
    Fixes #218

--- src/utils/components/background-image/classes.js
+++ src/utils/components/background-image/classes.js
@@ -5,7 +5,7 @@ import { dimRatioToClass } from './shared';
  */
 function BackgroundImageClasses( attributes ) {
 	return [
-		attributes.backgroundDimRatio && 100 !== attributes.backgroundDimRatio ? 'ab-has-background-dim' : null,
+		attributes.backgroundDimRatio !== undefined && 100 !== attributes.backgroundDimRatio ? 'ab-has-background-dim' : null,
 		dimRatioToClass( attributes.backgroundDimRatio ),
 		attributes.backgroundImgURL && attributes.backgroundSize && 'no-repeat' === attributes.backgroundRepeat ? 'ab-background-' + attributes.backgroundSize : null,
 		attributes.backgroundImgURL && attributes.backgroundRepeat ? 'ab-background-' + attributes.backgroundRepeat : null,
