27e3a9a1d76eabd59c0675e0ac90266d43de5bd9
Mike McAlister <m@mikemcalister.com>
2019-11-10 18:13:54 -0600

    Remove excessive control permissions

--- src/utils/components/margin.js
+++ src/utils/components/margin.js
@@ -1,8 +1,3 @@
-/**
- * External dependencies.
- */
-import RenderSettingControl from '../../utils/components/settings/renderSettingControl';
-
 const { __ } = wp.i18n;
 const { Fragment } = wp.element;
 const { RangeControl } = wp.components;
@@ -62,70 +57,58 @@ export default function Margin( props ) {
 	return (
 		<Fragment>
 			{ marginEnableTop && (
-				<RenderSettingControl id="ab_marginTop">
-					<RangeControl
-						label={ marginTopLabel ? marginTopLabel : __( 'Margin Top', 'atomic-blocks' ) }
-						value={ marginTop }
-						min={ marginTopMin }
-						max={ marginTopMax }
-						onChange={ onChangeMarginTop }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ marginTopLabel ? marginTopLabel : __( 'Margin Top', 'atomic-blocks' ) }
+					value={ marginTop }
+					min={ marginTopMin }
+					max={ marginTopMax }
+					onChange={ onChangeMarginTop }
+				/>
 			) }
 			{ marginEnableRight && (
-				<RenderSettingControl id="ab_marginRight">
-					<RangeControl
-						label={ marginRightLabel ? marginRightLabel : __( 'Margin Right', 'atomic-blocks' ) }
-						value={ marginRight }
-						min={ marginRightMin }
-						max={ marginRightMax }
-						onChange={ onChangeMarginRight }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ marginRightLabel ? marginRightLabel : __( 'Margin Right', 'atomic-blocks' ) }
+					value={ marginRight }
+					min={ marginRightMin }
+					max={ marginRightMax }
+					onChange={ onChangeMarginRight }
+				/>
 			) }
 			{ marginEnableBottom && (
-				<RenderSettingControl id="ab_marginBottom">
-					<RangeControl
-						label={ marginBottomLabel ? marginBottomLabel : __( 'Margin Bottom', 'atomic-blocks' ) }
-						value={ marginBottom }
-						min={ marginBottomMin }
-						max={ marginBottomMax }
-						onChange={ onChangeMarginBottom }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ marginBottomLabel ? marginBottomLabel : __( 'Margin Bottom', 'atomic-blocks' ) }
+					value={ marginBottom }
+					min={ marginBottomMin }
+					max={ marginBottomMax }
+					onChange={ onChangeMarginBottom }
+				/>
 			) }
 			{ marginEnableLeft && (
-				<RenderSettingControl id="ab_marginLeft">
-					<RangeControl
-						label={ marginLeftLabel ? marginLeftLabel : __( 'Margin Left', 'atomic-blocks' ) }
-						value={ marginLeft }
-						min={ marginLeftMin }
-						max={ marginLeftMax }
-						onChange={ onChangeMarginLeft }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ marginLeftLabel ? marginLeftLabel : __( 'Margin Left', 'atomic-blocks' ) }
+					value={ marginLeft }
+					min={ marginLeftMin }
+					max={ marginLeftMax }
+					onChange={ onChangeMarginLeft }
+				/>
 			) }
 			{ marginEnableVertical && (
-				<RenderSettingControl id="ab_marginVertical">
-					<RangeControl
-						label={ marginVerticalLabel ? marginVerticalLabel : __( 'Margin Vertical', 'atomic-blocks' ) }
-						value={ marginVertical }
-						min={ marginVerticalMin }
-						max={ marginVerticalMax }
-						onChange={ onChangeMarginVertical }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ marginVerticalLabel ? marginVerticalLabel : __( 'Margin Vertical', 'atomic-blocks' ) }
+					value={ marginVertical }
+					min={ marginVerticalMin }
+					max={ marginVerticalMax }
+					onChange={ onChangeMarginVertical }
+				/>
 			) }
 			{ marginEnableHorizontal && (
-				<RenderSettingControl id="ab_marginHorizontal">
-					<RangeControl
-						label={ marginHorizontalLabel ? marginHorizontalLabel : __( 'Margin Horizontal', 'atomic-blocks' ) }
-						value={ marginHorizontal }
-						min={ marginHorizontalMin }
-						max={ marginHorizontalMax }
-						onChange={ onChangeMarginHorizontal }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ marginHorizontalLabel ? marginHorizontalLabel : __( 'Margin Horizontal', 'atomic-blocks' ) }
+					value={ marginHorizontal }
+					min={ marginHorizontalMin }
+					max={ marginHorizontalMax }
+					onChange={ onChangeMarginHorizontal }
+				/>
 			) }
 		</Fragment>
 	);
--- src/utils/components/padding.js
+++ src/utils/components/padding.js
@@ -1,8 +1,3 @@
-/**
- * External dependencies.
- */
-import RenderSettingControl from '../../utils/components/settings/renderSettingControl';
-
 const { __ } = wp.i18n;
 const { Fragment } = wp.element;
 const { RangeControl } = wp.components;
@@ -65,82 +60,68 @@ export default function Padding( props ) {
 	return (
 		<Fragment>
 			{ paddingEnable && (
-				<RenderSettingControl id="ab_padding">
-					<RangeControl
-						label={ paddingTitle ? paddingTitle : __( 'Padding', 'atomic-blocks' ) }
-						help={ paddingHelp ? paddingHelp : null }
-						value={ padding }
-						min={ paddingMin }
-						max={ paddingMax }
-						onChange={ onChangePadding }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ paddingTitle ? paddingTitle : __( 'Padding', 'atomic-blocks' ) }
+					help={ paddingHelp ? paddingHelp : null }
+					value={ padding }
+					min={ paddingMin }
+					max={ paddingMax }
+					onChange={ onChangePadding }
+				/>
 			) }
 			{ paddingEnableTop && (
-				<RenderSettingControl id="ab_paddingTop">
-					<RangeControl
-						label={ __( 'Padding Top', 'atomic-blocks' ) }
-						value={ paddingTop }
-						min={ paddingTopMin }
-						max={ paddingTopMax }
-						onChange={ onChangePaddingTop }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ __( 'Padding Top', 'atomic-blocks' ) }
+					value={ paddingTop }
+					min={ paddingTopMin }
+					max={ paddingTopMax }
+					onChange={ onChangePaddingTop }
+				/>
 			) }
 			{ paddingEnableRight && (
-				<RenderSettingControl id="ab_paddingRight">
-					<RangeControl
-						label={ __( 'Padding Right', 'atomic-blocks' ) }
-						value={ paddingRight }
-						min={ paddingRightMin }
-						max={ paddingRightMax }
-						onChange={ onChangePaddingRight }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ __( 'Padding Right', 'atomic-blocks' ) }
+					value={ paddingRight }
+					min={ paddingRightMin }
+					max={ paddingRightMax }
+					onChange={ onChangePaddingRight }
+				/>
 			) }
 			{ paddingEnableBottom && (
-				<RenderSettingControl id="ab_paddingBottom">
-					<RangeControl
-						label={ __( 'Padding Bottom', 'atomic-blocks' ) }
-						value={ paddingBottom }
-						min={ paddingBottomMin }
-						max={ paddingBottomMax }
-						onChange={ onChangePaddingBottom }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ __( 'Padding Bottom', 'atomic-blocks' ) }
+					value={ paddingBottom }
+					min={ paddingBottomMin }
+					max={ paddingBottomMax }
+					onChange={ onChangePaddingBottom }
+				/>
 			) }
 			{ paddingEnableLeft && (
-				<RenderSettingControl id="ab_paddingLeft">
-					<RangeControl
-						label={ __( 'Padding Left', 'atomic-blocks' ) }
-						value={ paddingLeft }
-						min={ paddingLeftMin }
-						max={ paddingLeftMax }
-						onChange={ onChangePaddingLeft }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ __( 'Padding Left', 'atomic-blocks' ) }
+					value={ paddingLeft }
+					min={ paddingLeftMin }
+					max={ paddingLeftMax }
+					onChange={ onChangePaddingLeft }
+				/>
 			) }
 			{ paddingEnableVertical && (
-				<RenderSettingControl id="ab_paddingVertical">
-					<RangeControl
-						label={ __( 'Padding Vertical', 'atomic-blocks' ) }
-						value={ paddingVertical }
-						min={ paddingVerticalMin }
-						max={ paddingVerticalMax }
-						onChange={ onChangePaddingVertical }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ __( 'Padding Vertical', 'atomic-blocks' ) }
+					value={ paddingVertical }
+					min={ paddingVerticalMin }
+					max={ paddingVerticalMax }
+					onChange={ onChangePaddingVertical }
+				/>
 			) }
 			{ paddingEnableHorizontal && (
-				<RenderSettingControl id="ab_paddingHorizontal">
-					<RangeControl
-						label={ __( 'Padding Horizontal', 'atomic-blocks' ) }
-						value={ paddingHorizontal }
-						min={ paddingHorizontalMin }
-						max={ paddingHorizontalMax }
-						onChange={ onChangePaddingHorizontal }
-					/>
-				</RenderSettingControl>
+				<RangeControl
+					label={ __( 'Padding Horizontal', 'atomic-blocks' ) }
+					value={ paddingHorizontal }
+					min={ paddingHorizontalMin }
+					max={ paddingHorizontalMax }
+					onChange={ onChangePaddingHorizontal }
+				/>
 			) }
 		</Fragment>
 	);
