747f57cc30086f7491ac21e51ffd2cbe14cd12fa
Mike McAlister <m@mikemcalister.com>
2019-11-11 13:30:02 -0600

    Add setting controls to testimonial

--- src/blocks/block-testimonial/components/inspector.js
+++ src/blocks/block-testimonial/components/inspector.js
@@ -2,6 +2,8 @@
  * Inspector Controls
  */
 
+import RenderSettingControl from '../../../utils/components/settings/renderSettingControl';
+
 // Setup the block
 const { __ } = wp.i18n;
 const { Component } = wp.element;
@@ -46,44 +48,51 @@ export default class Inspector extends Component {
 		return (
 		<InspectorControls key="inspector">
 			<PanelBody>
-				<RangeControl
-					label={ __( 'Font Size', 'atomic-blocks' ) }
-					value={ testimonialFontSize }
-					onChange={ ( value ) => this.props.setAttributes({ testimonialFontSize: value }) }
-					min={ 14 }
-					max={ 24 }
-					step={ 1 }
-				/>
-
-				<SelectControl
-					label={ __( 'Cite Alignment', 'atomic-blocks' ) }
-					description={ __( 'Left or right align the cite name and title.', 'atomic-blocks' ) }
-					options={ citeAlignOptions }
-					value={ testimonialCiteAlign }
-					onChange={ ( value ) => this.props.setAttributes({ testimonialCiteAlign: value }) }
-				/>
+				<RenderSettingControl id="ab_testimonial_testimonialFontSize">
+					<RangeControl
+						label={ __( 'Font Size', 'atomic-blocks' ) }
+						value={ testimonialFontSize }
+						onChange={ ( value ) => this.props.setAttributes({ testimonialFontSize: value }) }
+						min={ 14 }
+						max={ 24 }
+						step={ 1 }
+					/>
+				</RenderSettingControl>
+				<RenderSettingControl id="ab_testimonial_testimonialCiteAlign">
+					<SelectControl
+						label={ __( 'Cite Alignment', 'atomic-blocks' ) }
+						description={ __( 'Left or right align the cite name and title.', 'atomic-blocks' ) }
+						options={ citeAlignOptions }
+						value={ testimonialCiteAlign }
+						onChange={ ( value ) => this.props.setAttributes({ testimonialCiteAlign: value }) }
+					/>
+				</RenderSettingControl>
 			</PanelBody>
-			<PanelColorSettings
-				title={ __( 'Background Color', 'atomic-blocks' ) }
-				initialOpen={ false }
-				colorSettings={ [ {
-					value: testimonialBackgroundColor,
-					onChange: onChangeBackgroundColor,
-					label: __( 'Background Color', 'atomic-blocks' )
-				} ] }
-			>
-			</PanelColorSettings>
+			<RenderSettingControl id="ab_testimonial_testimonialBackgroundColor">
+				<PanelColorSettings
+					title={ __( 'Background Color', 'atomic-blocks' ) }
+					initialOpen={ false }
+					colorSettings={ [ {
+						value: testimonialBackgroundColor,
+						onChange: onChangeBackgroundColor,
+						label: __( 'Background Color', 'atomic-blocks' )
+					} ] }
+				>
+				</PanelColorSettings>
+			</RenderSettingControl>
 
-			<PanelColorSettings
-				title={ __( 'Text Color', 'atomic-blocks' ) }
-				initialOpen={ false }
-				colorSettings={ [ {
-					value: testimonialTextColor,
-					onChange: onChangeTextColor,
-					label: __( 'Text Color', 'atomic-blocks' )
-				} ] }
-			>
-			</PanelColorSettings>
+			<RenderSettingControl id="ab_testimonial_testimonialTextColor">
+				<PanelColorSettings
+					title={ __( 'Text Color', 'atomic-blocks' ) }
+					initialOpen={ false }
+					colorSettings={ [ {
+						value: testimonialTextColor,
+						onChange: onChangeTextColor,
+						label: __( 'Text Color', 'atomic-blocks' )
+					} ] }
+				>
+				</PanelColorSettings>
+			</RenderSettingControl>
 		</InspectorControls>
 		);
 	}
