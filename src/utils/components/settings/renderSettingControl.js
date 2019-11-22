712676ee2410da5ccf4db13eaaff73dbc8eb7292
John Parris <john.parris@wpengine.com>
2019-11-22 08:16:43 -0600

    Add block name to filter.

--- src/utils/components/settings/renderSettingControl.js
+++ src/utils/components/settings/renderSettingControl.js
@@ -14,13 +14,26 @@ export default class RenderSettingControl extends Component {
 	render() {
 
 		/**
-		 * A filter for determing whether or not a setting should be rendered.
-		 *
-		 * @param Boolean Whether or not the setting control should be rendered. Default true.
-		 * @param String The setting control's ID.
-		 * @param Object The current user's data.
+		 * Let the temporary hacks begin.
+		 * Get the block name this setting is associated with.
 		 */
-		if ( applyFilters( 'ab_should_render_block_setting', true, this.props.id, getCurrentUserData() ) ) {
+		let fallback = false;
+
+		if ( typeof this.props.children === 'undefined' || typeof this.props.children.props === 'undefined' || typeof this.props.children.props.name === 'undefined' ) {
+			fallback = true;
+		}
+
+		let blockName = fallback ? this.props.children._owner.memoizedProps.name : this.props.children.props.name;
+
+		/**
+		 * A filter for determining whether or not a setting should be rendered.
+		 *
+		 * @param {boolean} Whether or not the setting control should be rendered. Default true.
+		 * @param {string} The block name.
+		 * @param {string} The setting control's ID.
+		 * @param {object} The current user's data.
+		 */
+		if ( applyFilters( 'ab_should_render_block_setting', true, blockName, this.props.id, getCurrentUserData() ) ) {
 			return this.props.children;
 		}
 
