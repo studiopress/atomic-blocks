4dc4a36198401b71ce46f7dee9393780377e4f25
John Parris <john.parris@wpengine.com>
2019-11-27 12:01:31 -0500

    Change way we detect HubSpot plugin

--- src/blocks/block-newsletter/index.php
+++ src/blocks/block-newsletter/index.php
@@ -37,7 +37,7 @@ function atomic_blocks_render_newsletter_block( $attributes ) {
 
 	$amp_endpoint = function_exists( 'is_amp_endpoint' ) && is_amp_endpoint();
 
-	$hubspot_installed = is_plugin_active( 'leadin/leadin.php' );
+	$hubspot_installed = defined( 'LEADIN_PLUGIN_VERSION' );
 
 	if ( ! $amp_endpoint && ! $hubspot_installed ) {
 		wp_enqueue_script( 'atomic-blocks-newsletter-functions' );
