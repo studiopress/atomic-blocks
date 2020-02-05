studiopress/atomic-blocks

commit 4b501ce578fd405567ff2b32a715a4162b45ee66

Register styles at wp_default_styles action and enqueue later as needed

dist/init.php
@@ -17,14 +17,16 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Enqueue assets for frontend and backend
  *
  * @since 1.0.0
+ *
+ * @param WP_Styles $wp_styles Styles.
  */
-function atomic_blocks_block_assets() {
+function atomic_blocks_block_assets( WP_Styles $wp_styles ) {
 
 	// phpcs:ignore WordPress.PHP.StrictComparisons.LooseComparison -- Could be true or 'true'.
 	$postfix = ( SCRIPT_DEBUG == true ) ? '' : '.min';
 
 	// Load the compiled styles.
-	wp_register_style(
+	$wp_styles->add(
 		'atomic-blocks-style-css',
 		plugins_url( 'dist/blocks.style.build.css', dirname( __FILE__ ) ),
 		array(),
@@ -32,14 +34,14 @@ function atomic_blocks_block_assets() {
 	);
 
 	// Register the FontAwesome icon library.
-	wp_register_style(
+	$wp_styles->add(
 		'atomic-blocks-fontawesome',
 		plugins_url( 'dist/assets/fontawesome/css/all' . $postfix . '.css', dirname( __FILE__ ) ),
 		array(),
 		filemtime( plugin_dir_path( __FILE__ ) . 'assets/fontawesome/css/all.css' )
 	);
 }
-add_action( 'init', 'atomic_blocks_block_assets' );
+add_action( 'wp_default_styles', 'atomic_blocks_block_assets' );
 
 /**
  * Conditionally print Font Awesome stylesheet the first time it is needed by a block.
@@ -92,12 +94,7 @@ function atomic_blocks_editor_assets() {
 	);
 
 	// Load the FontAwesome icon library.
-	wp_enqueue_style(
-		'atomic-blocks-fontawesome',
-		plugins_url( 'dist/assets/fontawesome/css/all' . $postfix . '.css', dirname( __FILE__ ) ),
-		array(),
-		filemtime( plugin_dir_path( __FILE__ ) . 'assets/fontawesome/css/all.css' )
-	);
+	wp_enqueue_style( 'atomic-blocks-fontawesome' );
 
 	$user_data = wp_get_current_user();
 	unset( $user_data->user_pass, $user_data->user_email );

dist/getting-started/getting-started.php
@@ -34,7 +34,6 @@ function atomic_blocks_start_load_admin_scripts( $hook ) {
 	wp_enqueue_style( 'atomic-blocks-getting-started' );
 
 	// FontAwesome.
-	wp_register_style( 'atomic-blocks-fontawesome', plugins_url( '/assets/fontawesome/css/all' . $postfix . '.css', dirname( __FILE__ ) ), false, '1.0.0' );
 	wp_enqueue_style( 'atomic-blocks-fontawesome' );
 }
 add_action( 'admin_enqueue_scripts', 'atomic_blocks_start_load_admin_scripts' );

commit 5b3dfe911785561dcef3cb5ed11ed09ed993c715

Do not attempt to inline fontawesome CSS in the editor
Fixes the issue noted in #127 and introduced in #294, where icons
in existing AB Sharing blocks will fail to load in the editor if REST
requests return other pages that have an AB Sharing block.

dist/init.php
@@ -50,6 +50,10 @@ add_action( 'wp_default_styles', 'atomic_blocks_block_assets' );
  * @return string Block content with Font Awesome stylesheet prepended if needed.
  */
 function atomic_blocks_prepend_block_content_with_fontawesome( $block_content ) {
+	if ( is_admin() ) {
+		return $block_content;
+	}
+
 	$handle = 'atomic-blocks-fontawesome';
 	if (
 		! wp_style_is( $handle, 'done' )