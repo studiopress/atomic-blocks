955589b95da5ffb2b27dc98bbd274f34ffa8f1f5
John Parris <john.parris@wpengine.com>
2020-02-27 09:02:42 -0500

    Add Nodelist.forEach polyfill for IE11. #236

--- dist/assets/js/dismiss.js
+++ dist/assets/js/dismiss.js
@@ -2,6 +2,15 @@
  * Handles dismissible notices from the Notice block.
  */
 
+/**
+ * IE 11 polyfill for Nodelist.forEach.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/NodeList/forEach
+ */
+if ( window.NodeList && ! NodeList.prototype.forEach ) {
+	NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
 document.addEventListener( 'DOMContentLoaded', function() {
 
 	var notices = document.querySelectorAll( '.ab-block-notice.ab-dismissable[data-id]' );
