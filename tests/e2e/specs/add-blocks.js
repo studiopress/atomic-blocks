b103b2e68aa99e8582a0c80cee4c5d2b783be6db
Ryan Kienstra <kienstraryan@gmail.com>
2022-01-14 11:41:31 -0600

    Use insertBlock() from the Gutenberg npm package

--- tests/e2e/specs/add-blocks.js
+++ tests/e2e/specs/add-blocks.js
@@ -3,33 +3,9 @@
  */
 import {
 	createNewPost,
-	openGlobalBlockInserter,
-	pressKeyWithModifier,
+	insertBlock,
 	saveDraft,
 } from '@wordpress/e2e-test-utils';
-
-/**
- * Inserts a block from the block inserter, mainly copied from Gutenberg.
- *
- * @see https://github.com/WordPress/gutenberg/blob/56f912adc681ebd3a6fb6a17eb4cfcb2c0050f5b/packages/e2e-test-utils/src/insert-block.js
- *
- * @param {string} blockName The block name to insert.
- */
-const insertBlock = async ( blockName ) => {
-	await openGlobalBlockInserter();
-	await page.waitForSelector( '.block-editor-inserter__menu' );
-	await page.focus( '[placeholder="Search for a block"]' );
-	await pressKeyWithModifier( 'primary', 'a' );
-	await page.keyboard.type( blockName );
-	const insertButton = (
-		await page.$x( `//button//span[contains(text(), '${ blockName }')]` )
-	)[ 0 ];
-	await insertButton.click();
-
-	const editorSelector = '.edit-post-layout';
-	await page.waitForSelector( editorSelector );
-	await page.click( editorSelector )
-};
 
 describe( 'Add Blocks', () => {
 	it( 'inserts blocks with the inserter', async () => {
